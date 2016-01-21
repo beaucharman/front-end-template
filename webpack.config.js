ef5e41e5428af3f98ba9d8f5543726ab9f4ebbb6
Beau Charman <beau.charman@gmail.com>
2016-01-21 17:47:35 +1100

    update webpack + add support for url / images

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var path = require('path')
 
 module.exports = {
   entry: './app/entry.jsx',
   output: {
     filename: './static/bundle.js',
+    publicPath: '/',
   },
   plugins: [
     new ExtractTextPlugin('./static/app.css', {
@@ -22,12 +24,23 @@ module.exports = {
       },
       {
         test: /\.styl$/,
-        loader: ExtractTextPlugin.extract("style-loader", "css-loader?module&localIdentName=[path][name]---[local]---[hash:base64:5]!stylus-loader"),
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?module&localIdentName=[name]---[local]---[hash:base64:5]!stylus-loader'),
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?name=[name].[ext]&limit=8192&path=static',
       },
     ]
   },
   stylus: {
-    use: [require('nib')()],
-    import: ['~nib/lib/nib/index.styl'],
+    use: [
+      require('nib')(),
+      require('rupture')(),
+    ],
+    import: [
+      '~nib/lib/nib/index.styl',
+      '~rupture/rupture/index.styl',
+      path.join(path.join(__dirname, 'app') + '/variables.styl'),
+    ],
   },
 }
--- webpack/constants.js
+++
@@ -1,7 +0,0 @@
-var path = require('path')
-
-module.exports = {
-  ROOT_DIR: path.join(__dirname, '..'),
-  APP_DIR: path.join(__dirname, '..', 'app'),
-  DIST_DIR: path.join(__dirname, '..', 'static'),
-}
