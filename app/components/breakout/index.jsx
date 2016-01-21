ba2e080ac9ebad36d88a72482f7cdef1fd6ac2b5
Beau Charman <beau.charman@gmail.com>
2016-01-21 18:16:24 +1100

    Breakout, Content and Gallery Image component placeholders

---
+++ app/components/breakout/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Breakout extends Component {
+  render() {
+    return (
+      <h1>Breakout placeholder</h1>
+    )
+  }
+}
+
+export default Breakout
---
+++ app/components/gallery-image/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class GalleryImage extends Component {
+  render() {
+    return (
+      <h1>Gallery Image placeholder</h1>
+    )
+  }
+}
+
+export default GalleryImage
